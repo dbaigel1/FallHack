a5fe6402133ec071bf2bb6c8df2cbeafd3e84fb5
Kenneth Postigo <kpostigo@minusplus.local>
2014-10-24 20:41:04 -0400

    update

--- splyce.js
+++ splyce.js
@@ -1,24 +1,23 @@
-<SCRIPT LANGUAGE = "JavaScript">
-	var apiKey = "JYT16WJLIG6YRMXXT";
-	var request = new XMLHttpRequest();
+var apiKey = "JYT16WJLIG6YRMXXT";
+var request = new XMLHttpRequest();
 	
-	function getSongs(){
+function getSongs(){
 		
-		var artist;
+	var artist;
 		
-		artist = document.getElementById("artist").value;
-		request.open("GET", "http://developer.echonest.com/api/v4/artist/songs?", true);
-		request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		request.onreadystatechange = function () {
-			if (request.readyState == 4) {
-				console.log(request.responseText);
-			}
+	artist = document.getElementById("artist").value;
+		
+	request.open("GET", "http://developer.echonest.com/api/v4/artist/songs?", true);
+	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	request.onreadystatechange = function () {
+		if (request.readyState == 4) {
+			console.log(request.responseText);
 		}
-		request.send("api_key="+apiKey+"&name=Weezer");
 	}
+	request.send("api_key="+apiKey+"&name=Weezer");
+	
+}
 
-	function printSongs(){
-		// FUNCTION HERE
-	}
-
-</SCRIPT>
+function printSongs(){
+	// FUNCTION HERE
+}
